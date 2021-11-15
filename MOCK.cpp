e3bf602b4b5be66568d0253bdbd942a7acf4a398
phong13082000 <84502367+phong13082000@users.noreply.github.com>
2021-11-16 01:03:53 +0700

    Update MOCK.cpp

--- MOCK.cpp
+++ MOCK.cpp
@@ -1,11 +1,35 @@
 #include<iostream>
-#include<conio.h>
-#include <windows.h>
 using namespace std;
-void gotoxy(char& a[][], int x, int y)
-{	
-	a[x][y]='X';
-	cout <<	a[x][y];
+// Cho phep nguoi dung nhap cac buoc di chuyen bang dong lenh
+// Hien thi trang thai hien tai cua board
+// Kiem tra tinh trang win va bao cao ket qua
+// Tiep tuc cho den khi 2 nguoi choi thang ( chua lam dieu kien hoa )
+// Chua tao bot, chua tao chuc nang xem lai, chua luu thong tin nguoi choi
+void checkWin(char a[23][45]){
+	for(int i=0; i<23; i++){
+		for(int j=0; j<45; j++){
+			if(a[i][j]==a[i+2][j] && a[i+2][j]==a[i+4][j] && a[i+4][j]==a[i+6][j] && a[i+6][j]==a[i+8][j] &&  a[i+8][j]=='X'){
+				cout << "Player 1 win !" << endl;
+				break;
+			}else if(a[i][j]==a[i+2][j] && a[i+2][j]==a[i+4][j] && a[i+4][j]==a[i+6][j] && a[i+6][j]==a[i+8][j] && a[i+8][j]=='O'){
+				cout << "Player 2 win !" << endl;
+				break;	
+			}else if(a[i][j]==a[i][j+4] && a[i][j+4]==a[i][j+8] && a[i][j+8]==a[i][j+12] && a[i][j+12]==a[i][j+16] && a[i][j+16]=='X'){
+				cout << "Player 1 win !" << endl;
+				break;
+			}else if(a[i][j]==a[i][j+4] && a[i][j+4]==a[i][j+8] && a[i][j+8]==a[i][j+12] && a[i][j+12]==a[i][j+16] && a[i][j+16]=='O'){
+				cout << "Player 2 win !" << endl;
+				break;
+			}
+			else if(a[i][j]==a[i+2][j+4] && a[i+2][j+4]==a[i+4][j+8] && a[i+4][j+8]==a[i+6][j+12] && a[i+6][j+12]==a[i+8][j+16] && a[i+8][j+16]=='X'){
+				cout << "Player 1 win !" << endl;
+				break;
+			}else if(a[i][j]==a[i+2][j+4] && a[i+2][j+4]==a[i+4][j+8] && a[i+4][j+8]==a[i+6][j+12] && a[i+6][j+12]==a[i+8][j+16] && a[i+8][j+16]=='O'){
+				cout << "Player 2 win !" << endl;
+				break;
+			}
+		}
+	}
 }
 int main(){
 	char a[23][45];
@@ -49,14 +73,36 @@ int main(){
 		}
 		cout << endl;
 	}
+	while(1){
 	cout << "Player 1's turn: " << endl;
 	cout << "Column: ";
 	cin >> m;
 	cout << "Row: ";
 	cin >> n;
-	c = m*4+6;
-	r = n*2+3;
-	gotoxy(a[23][45],r,c);
-	getch();
-	
+	for(int i=0; i<23; i++){
+		for(int j=0; j<45; j++){
+			c = m*4+6;
+			r = n*2+3;
+			a[r][c] = 'X';
+			cout << a[i][j];
+		}
+		cout << endl;
+	}
+	checkWin(a);
+	cout << "Player 2's turn: " << endl;
+	cout << "Column: ";
+	cin >> m;
+	cout << "Row: ";
+	cin >> n;
+	for(int i=0; i<23; i++){
+		for(int j=0; j<45; j++){
+			c = m*4+6;
+			r = n*2+3;
+			a[r][c] = 'O';
+			cout << a[i][j];
+		}
+		cout << endl;
+	}
+	checkWin(a);
+}
 }
