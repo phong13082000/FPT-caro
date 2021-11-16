c9fedd0e4a123aa25a1ee643669581c6164d8516
phong13082000 <84502367+phong13082000@users.noreply.github.com>
2021-11-16 12:47:51 +0700

    Update MOCK.cpp

--- MOCK.cpp
+++ MOCK.cpp
@@ -5,36 +5,60 @@ using namespace std;
 // Kiem tra tinh trang win va bao cao ket qua
 // Tiep tuc cho den khi 2 nguoi choi thang ( chua lam dieu kien hoa )
 // Chua tao bot, chua tao chuc nang xem lai, chua luu thong tin nguoi choi
-int checkWin(char a[23][45]){
+int checkWin(char a[23][45])
+{
 	for(int i=0; i<23; i++){
 		for(int j=0; j<45; j++){
-			if(a[i][j]==a[i+2][j] && a[i+2][j]==a[i+4][j] && a[i+4][j]==a[i+6][j] && a[i+6][j]=='X'){
+			if(a[i][j]==a[i+2][j] && a[i+2][j]==a[i+4][j] && a[i+4][j]==a[i+6][j] && a[i+6][j]=='X')
+			{
 				return 1;
 				break;
-			}else if(a[i][j]==a[i+2][j] && a[i+2][j]==a[i+4][j] && a[i+4][j]==a[i+6][j] && a[i+6][j]=='O'){
+			}else if(a[i][j]==a[i+2][j] && a[i+2][j]==a[i+4][j] && a[i+4][j]==a[i+6][j] && a[i+6][j]=='O')
+			{
 				return 2;
 				break;	
-			}else if(a[i][j]==a[i][j+4] && a[i][j+4]==a[i][j+8] && a[i][j+8]==a[i][j+12] && a[i][j+12]=='X'){
+			}else if(a[i][j]==a[i][j+4] && a[i][j+4]==a[i][j+8] && a[i][j+8]==a[i][j+12] && a[i][j+12]=='X')
+			{
 				return 1;
 				break;
-			}else if(a[i][j]==a[i][j+4] && a[i][j+4]==a[i][j+8] && a[i][j+8]==a[i][j+12] && a[i][j+12]=='O'){
+			}else if(a[i][j]==a[i][j+4] && a[i][j+4]==a[i][j+8] && a[i][j+8]==a[i][j+12] && a[i][j+12]=='O')
+			{
 				return 2;
 				break;
-			}else if(a[i][j]==a[i+2][j+4] && a[i+2][j+4]==a[i+4][j+8] && a[i+4][j+8]==a[i+6][j+12] && a[i+6][j+12]=='X'){
+			}else if(a[i][j]==a[i+2][j+4] && a[i+2][j+4]==a[i+4][j+8] && a[i+4][j+8]==a[i+6][j+12] && a[i+6][j+12]=='X')
+			{
 				return 1;
 				break;
-			}else if(a[i][j]==a[i+2][j+4] && a[i+2][j+4]==a[i+4][j+8] && a[i+4][j+8]==a[i+6][j+12] && a[i+6][j+12]=='O'){
+			}else if(a[i][j]==a[i+2][j+4] && a[i+2][j+4]==a[i+4][j+8] && a[i+4][j+8]==a[i+6][j+12] && a[i+6][j+12]=='O')
+			{
 				return 2;
 				break;
-			}else if(a[i][j]==a[i+2][j-4] && a[i+2][j-4]==a[i+4][j-8] && a[i+4][j-8]==a[i+6][j-12] && a[i+6][j-12]=='X'){
+			}else if(a[i][j]==a[i+2][j-4] && a[i+2][j-4]==a[i+4][j-8] && a[i+4][j-8]==a[i+6][j-12] && a[i+6][j-12]=='X')
+			{
 				return 1;
 				break;
-			}else if(a[i][j]==a[i+2][j-4] && a[i+2][j-4]==a[i+4][j-8] && a[i+4][j-8]==a[i+6][j-12] && a[i+6][j-12]=='O'){
+			}else if(a[i][j]==a[i+2][j-4] && a[i+2][j-4]==a[i+4][j-8] && a[i+4][j-8]==a[i+6][j-12] && a[i+6][j-12]=='O')
+			{
 				return 2;
 				break;
 			}
 		}
 	}
+}
+bool Full(char a[23][45])
+{
+	int dem=0;
+	for(int i=0; i<23; i++)
+	{
+		for(int j=0; j<45; j++)
+		{
+			if ((a[i][j]=='X') || (a[i][j]=='O'))
+			dem++;
+		}
+	}
+	if(dem==100) 	
+		return true;
+		return false;
 }
 void BanCo(char a[23][45], int x=0, int y=0)
 {
@@ -140,35 +164,45 @@ int main(){
     	cout << "Press number to choice: ";
     	cin >> number;
     	int r1=0,c1=0,r2=0,c2=0;
-	    switch(number){
-	    	case 1:{
+	    switch(number)
+		{
+	    	case 1:
 	    		BanCo(a,x,y);
 		    	while(1){
                 	player1(a,m,n);
-                	if(checkWin(a)==1){
+                	if(checkWin(a)==1)
+					{
                 		cout << "Player 1 win !" << endl;
                 		break;
 	    			}
                 	player2(a,m,n);
-                	if(checkWin(a)==2){
+                	if(checkWin(a)==2)
+					{
                 		cout << "Player 2 win !" << endl;
                 		break;
     				}
+    				if(Full(a))
+					{
+    					cout <<"Draw!" <<endl;
+    					break;
+					}
                 }
     			break;
-    		}
-    		case 3:{
-    		    for(int i=0; i<23; i++){
-		            for(int j=0; j<45; j++){
+    		
+    		case 3:
+    		    for(int i=0; i<23; i++)
+				{
+		            for(int j=0; j<45; j++)
+					{
 		             	cout << a[i][j];
 	               	}
                		cout << endl;
                	}
 	    		break;
-	   		}
-    		case 6:{
+	   		
+    		case 6:
 		    	break;
-	    	}
+	    	
     	}
     	cout << "EXIT ? (Y/N): ";    
     	cin >> kt;
