7d31067a7f0bae7527ee2a3b5b8086ffaeb466f1
TuanHungPham <94421733+TuanHungPham@users.noreply.github.com>
2021-11-16 12:49:55 +0700

    Update MOCK.cpp

--- MOCK.cpp
+++ MOCK.cpp
@@ -117,8 +117,17 @@ void player1(char a[23][45],int m,int n)
 	cout << "Player 1's turn: " << endl;
 	cout << "Row: ";
 	cin >> n;
-	cout << "Column: ";
+	cout << "Colomn: ";
 	cin >> m;
+	while(m<0 || n<0 || a[n*2+3][m*4+6]=='X' || a[n*2+3][m*4+6]=='O' || m>23 || n>45)
+	{
+		//kiem tra o nhap co hop le hay khong
+		cout<<"Nhap lai o can danh"<<endl;
+		cout << "Row: ";
+		cin >> n;
+		cout << "Colomn: ";
+		cin >> m;
+	}
 	system("cls");
 	for(int i=0; i<23; i++){
 		for(int j=0; j<45; j++){
@@ -134,8 +143,17 @@ void player2(char a[23][45],int m,int n)
 	cout << "Player 2's turn: " << endl;
 	cout << "Row: ";
 	cin >> n;
-	cout << "Column: ";
+	cout << "Colomn: ";
 	cin >> m;
+	while(m<0 || n<0 || a[n*2+3][m*4+6]=='X' || a[n*2+3][m*4+6]=='O' || m>23 || n>45)
+	{
+		//kiem tra o nhap co hop le hay khong
+		cout<<"Nhap lai o can danh"<<endl;
+		cout << "Row: ";
+		cin >> n;
+		cout << "Colomn: ";
+		cin >> m;
+	}
 	system("cls");
 	for(int i=0; i<23; i++){
 		for(int j=0; j<45; j++){
